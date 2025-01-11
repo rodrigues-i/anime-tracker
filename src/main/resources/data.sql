f42cd8a1cab6fd5e754d99e86570b0255cc379f4
rodrigues-i <rodrigues-i@outlook.com>
2025-01-10 21:12:23 -0300

    Implement data seeding

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO anime (id, name, author, genre, num_seasons, creation_year, is_ongoing)
+VALUES
+    (1, 'Naruto', 'Masashi Kishimoto', 'Shonen', 5, '2002-10-03', true),
+    (2, 'Attack on Titan', 'Hajime Isayama', 'Action', 4, '2009-09-09', false),
+    (3, 'One Piece', 'Eiichiro Oda', 'Adventure', 10, '1999-10-20', true);
