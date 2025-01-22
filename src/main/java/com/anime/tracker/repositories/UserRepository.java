c8a7e0f926aba827324d006a7c6ff016044ec83d
rodrigues-i <rodrigues-i@outlook.com>
2025-01-22 20:20:35 -0300

    Create an abstract method

--- src/main/java/com/anime/tracker/repositories/UserRepository.java
+++ src/main/java/com/anime/tracker/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package com.anime.tracker.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.anime.tracker.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByUsername(String username);
 
 }
