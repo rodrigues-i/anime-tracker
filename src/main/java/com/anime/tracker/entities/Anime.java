fb5a823d52646732c01c4ed5883ea2ab59bc066c
rodrigues-i <rodrigues-i@outlook.com>
2025-01-22 20:19:04 -0300

    Rename table

--- src/main/java/com/anime/tracker/entities/Anime.java
+++ src/main/java/com/anime/tracker/entities/Anime.java
@@ -4,11 +4,10 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "animes")
 public class Anime extends Item
 {
 	private String author;
