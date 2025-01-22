21a75a14d9fb1073287fcdb4510214ce7bb99f03
rodrigues-i <rodrigues-i@outlook.com>
2025-01-22 20:15:50 -0300

    Add username and password fields in entity User

--- src/main/java/com/anime/tracker/entities/User.java
+++ src/main/java/com/anime/tracker/entities/User.java
@@ -1,17 +1,21 @@
 package com.anime.tracker.entities;
 
 import java.util.Objects;
+import java.util.Set;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "USERS")
+@Table(name = "users")
 public class User {
 
 	@Id
@@ -19,14 +23,20 @@ public class User {
 	private Long id;
 	private String name;
 	private Gender gender;
-	@Enumerated(EnumType.ORDINAL)
-	private Role role;
+	@Column(nullable = false, unique = true)
+	private String username;
+	@Column(nullable = false)
+	private String password;
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "users_roles",
+			joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+	private Set<Role> roles;
 
 	public User() {}
 
-	public User(Long id, String name, Gender gender) {
+	public User(String name, Gender gender, String username, String password, Set<Role> roles) {
 		super();
-		this.id = id;
 		this.name = name;
 		this.gender = gender;
 	}
@@ -43,8 +53,8 @@ public class User {
 		return name;
 	}
 
-	public Role getRole() {
-		return this.role;
+	public Set<Role> getRoles() {
+		return this.roles;
 	}
 
 	public void setName(String name) {
@@ -55,18 +65,34 @@ public class User {
 		return gender;
 	}
 
+	public String getUsername() {
+		return this.username;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
 	public void setGender(Gender gender) {
 		this.gender = gender;
 	}
 
-	public void setRole(Role role)
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setRoles(Set<Role> roles)
 	{
-		this.role = role;
+		this.roles = roles;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(gender, id, name, role);
+		return Objects.hash(gender, id, name, roles);
 	}
 
 	@Override
@@ -79,11 +105,11 @@ public class User {
 			return false;
 		User other = (User) obj;
 		return gender == other.gender && Objects.equals(id, other.id) && Objects.equals(name, other.name)
-				&& Objects.equals(role, other.role);
+				&& Objects.equals(roles, other.roles);
 	}
 
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", gender=" + gender + ", role=" + role + "]";
+		return "User [id=" + id + ", name=" + name + ", gender=" + gender + ", role=" + roles + "]";
 	}
 }
